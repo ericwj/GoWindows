ca4155bf033e97ac762b38edcb7e1e039f3a02e5
Liam <github@networkimprov.net>
2020-07-09 00:44:44 -0700

    api: Add all filepath APIs; change tInput.Api names to "filepath.*"

--- src/go/api.go
+++ src/go/api.go
@@ -12,6 +12,9 @@ import (
 type tInput struct {
    Api string
    Path string
+   Path2 string   // filepath.Rel
+   Pattern string // filepath.Glob & .Match
+   Join []string  // filepath.Join
 }
 
 type tOutput struct {
@@ -35,16 +38,30 @@ func main() {
       var aOut tOutput
 
       switch aIn.Api {
-      case "path.filepath.IsAbs":
-         aOut.Result = filepath.IsAbs(aIn.Path)
-      case "path.filepath.Abs":
-         aOut.Result, err = filepath.Abs(aIn.Path)
-      case "path.filepath.Walk":
+      case "filepath.Separator":     aOut.Result = filepath.Separator
+      case "filepath.ListSeparator": aOut.Result = filepath.ListSeparator
+      case "filepath.Abs":           aOut.Result, err = filepath.Abs(aIn.Path)
+      case "filepath.EvalSymlinks":  aOut.Result, err = filepath.EvalSymlinks(aIn.Path)
+      case "filepath.Glob":          aOut.Result, err = filepath.Glob(aIn.Pattern)
+      case "filepath.Match":         aOut.Result, err = filepath.Match(aIn.Pattern, aIn.Path)
+      case "filepath.Rel":           aOut.Result, err = filepath.Rel(aIn.Path, aIn.Path2)
+      case "filepath.Base":          aOut.Result = filepath.Base(aIn.Path)
+      case "filepath.Clean":         aOut.Result = filepath.Clean(aIn.Path)
+      case "filepath.Dir":           aOut.Result = filepath.Dir(aIn.Path)
+      case "filepath.Ext":           aOut.Result = filepath.Ext(aIn.Path)
+      case "filepath.FromSlash":     aOut.Result = filepath.FromSlash(aIn.Path)
+      case "filepath.IsAbs":         aOut.Result = filepath.IsAbs(aIn.Path)
+      case "filepath.Join":          aOut.Result = filepath.Join(aIn.Join...)
+      case "filepath.SplitList":     aOut.Result = filepath.SplitList(aIn.Path)
+      case "filepath.ToSlash":       aOut.Result = filepath.ToSlash(aIn.Path)
+      case "filepath.VolumeName":    aOut.Result = filepath.VolumeName(aIn.Path)
+      case "filepath.Split":         aOut.Result, aOut.Name = filepath.Split(aIn.Path)
+      case "filepath.Walk":
          err = filepath.Walk(aIn.Path, walkInfo)
          if err != nil { quit(err) }
          continue
       default:
-         quit(fmt.Errorf("Api not supported\n"))
+         quit(fmt.Errorf("Api not supported: %s\n", aIn.Api))
       }
       if err != nil {
          setError(&aOut, err)
