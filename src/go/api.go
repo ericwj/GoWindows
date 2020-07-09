a303d49753509fe9060debaa2a7cca29c88abb3f
Liam <github@networkimprov.net>
2020-07-08 20:15:14 -0700

    api: Add looping on stdin, fix Walk

--- src/go/api.go
+++ src/go/api.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-   "encoding/json"
    "path/filepath"
    "fmt"
+   "io"
+   "encoding/json"
    "os"
    "syscall"
 )
@@ -21,46 +22,57 @@ type tOutput struct {
 }
 
 var sJsonOut = json.NewEncoder(os.Stdout)
+var sJsonIn = json.NewDecoder(os.Stdin)
 
 func main() {
-   var aIn tInput
-   err := json.NewDecoder(os.Stdin).Decode(&aIn)
-   if err != nil { quit(err) }
-   var aOut tOutput
-
-   switch aIn.Api {
-   case "path.filepath.IsAbs":
-      aOut.Result = filepath.IsAbs(aIn.Path)
-   case "path.filepath.Abs":
-      aOut.Result, err = filepath.Abs(aIn.Path)
-   case "path.filepath.Walk":
-      err = filepath.Walk(aIn.Path, walkInfo)
-      if err != nil { quit(err) }
-      os.Exit(0)
-   default:
-      fmt.Fprintf(os.Stderr, "Api not supported\n")
-      os.Exit(1)
-   }
-   if err != nil {
-      aOut.Error = err.Error()
-      switch aV := err.(type) {
-      case syscall.Errno: aOut.Errno = int(aV)
-      case *os.PathError: aOut.Errno = int(aV.Err.(syscall.Errno))
-      default:            aOut.Errno = -111
+   for {
+      var aIn tInput
+      err := sJsonIn.Decode(&aIn)
+      if err != nil {
+         if err != io.EOF { quit(err) }
+         os.Exit(0)
       }
+      var aOut tOutput
+
+      switch aIn.Api {
+      case "path.filepath.IsAbs":
+         aOut.Result = filepath.IsAbs(aIn.Path)
+      case "path.filepath.Abs":
+         aOut.Result, err = filepath.Abs(aIn.Path)
+      case "path.filepath.Walk":
+         err = filepath.Walk(aIn.Path, walkInfo)
+         if err != nil { quit(err) }
+         continue
+      default:
+         quit(fmt.Errorf("Api not supported\n"))
+      }
+      if err != nil {
+         setError(&aOut, err)
+      }
+      err = sJsonOut.Encode(aOut)
+      if err != nil { quit(err) }
    }
-   err = sJsonOut.Encode(aOut)
-   if err != nil { quit(err) }
-   os.Exit(0)
 }
 
 func walkInfo(iPath string, iFi os.FileInfo, iErr error) error {
-   aOut := tOutput{Result: iPath, Name: iFi.Name()}
+   aOut := tOutput{Result: iPath}
+   if iFi != nil {
+      aOut.Name = iFi.Name()
+   }
    if iErr != nil {
-      aOut.Error, aOut.Errno = iErr.Error(), -222
+      setError(&aOut, iErr)
    }
    err := sJsonOut.Encode(aOut)
    return err
+}
+
+func setError(iOut *tOutput, iErr error) {
+   iOut.Error = iErr.Error()
+   switch aV := iErr.(type) {
+   case syscall.Errno: iOut.Errno = int(aV)
+   case *os.PathError: iOut.Errno = int(aV.Err.(syscall.Errno))
+   default:            iOut.Errno = -111
+   }
 }
 
 func quit(i error) {
